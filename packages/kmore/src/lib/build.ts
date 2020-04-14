2a7d838019ce8a2539d7d80c148768aa3cf1ef30
waiting <1661926154@qq.com>
2020-04-14 19:34:18 +0800

    feat(kmore): add lib/build.ts
    for building kTables file

---
+++ packages/kmore/src/lib/build.ts
@@ -0,0 +1,115 @@
+import { mergeMap } from 'rxjs/operators'
+import { Observable, defer } from 'rxjs'
+import {
+  walkDirForCallerFuncTsFiles,
+  retrieveTypeFromTsFile,
+  genTsCodeFromTypes,
+  saveFile,
+  TablesMapArr,
+  KTablesBase,
+  genColsTsCodeFromTypes,
+  DbPropKeys,
+} from 'kmore-types'
+
+import {
+  TTables,
+  FilePath,
+  CallerTbListMap,
+  BuildSrcOpts,
+  KTables,
+} from './model'
+import { initBuildSrcOpts } from './config'
+import { genKTablesFromBase } from './util'
+
+
+/**
+ * Generate tables .ts files,
+ * no path value emitted if no file generated.
+ */
+export function buildSource(options: BuildSrcOpts): Observable<FilePath> {
+  const opts: Required<BuildSrcOpts> = {
+    ...initBuildSrcOpts,
+    ...options,
+  }
+
+  const walk$ = walkDirForCallerFuncTsFiles(opts)
+  const build$ = walk$.pipe(
+    mergeMap((path) => {
+      return defer(() => buildKTablesFile(path, opts))
+    }, opts.concurrent),
+    // defaultIfEmpty(''),
+  )
+
+  return build$
+}
+
+
+
+/**
+ * Build tables in ts from generics type for specified ts file
+ *
+ * @returns file path if src file need parsed
+ */
+export async function buildKTablesFile<T extends TTables>(
+  file: string,
+  options: BuildSrcOpts,
+): Promise<FilePath> {
+
+  const opts: Required<BuildSrcOpts> = {
+    ...initBuildSrcOpts,
+    ...options,
+  }
+
+  let path = ''
+  let content = ''
+  const map: CallerTbListMap<T> = retrieveTypeFromTsFile<T>(file)
+
+  if (map && map.size) {
+    map.forEach((arr, key) => {
+      const [str, code] = genTsCodeFromTypes<T>(key, arr, opts)
+      if (! path) {
+        path = str // all value are the same one
+      }
+      content += code + '\n\n'
+
+      const kts = genKTables(arr)
+      const [, code2] = genColsTsCodeFromTypes<T>(
+        key,
+        kts.scopedColumns,
+        opts.exportVarPrefix,
+        DbPropKeys.scopedColumns,
+        opts.outputFileNameSuffix,
+      )
+      content += code2 + '\n\n'
+
+
+      const [, code3] = genColsTsCodeFromTypes<T>(
+        key,
+        kts.aliasColumns,
+        opts.exportVarPrefix,
+        DbPropKeys.aliasColumns,
+        opts.outputFileNameSuffix,
+      )
+      content += code3 + '\n\n'
+    })
+
+    if (! path) {
+      throw new Error('path value is empty')
+    }
+    await saveFile(path, content, opts.outputBanner)
+    path = path.replace(/\\/ug, '/')
+  }
+
+  return path
+}
+
+function genKTables<T extends TTables>(arr: TablesMapArr<T>): KTables<T> {
+  const [tables, columns] = arr
+  const kTabesBase: KTablesBase<T> = {
+    tables,
+    columns,
+  }
+  const kts = genKTablesFromBase(kTabesBase)
+  return kts
+}
+
