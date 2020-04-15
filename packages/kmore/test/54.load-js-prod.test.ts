bfbbfd1a9b6436afb7b2e1b8e055d38803049e10
waiting <1661926154@qq.com>
2020-04-15 11:13:22 +0800

    test(kmore): update

--- packages/kmore/test/54.load-js-prod.test.ts
+++ packages/kmore/test/54.load-js-prod.test.ts
@@ -6,12 +6,12 @@ import { tbList2 } from './config/test.config2'
 
 const filename = basename(__filename)
 
-describe(filename, () => {
+describe.only(filename, () => {
 
   describe('Should loading tables works', () => {
     it('normal', () => {
       assert(tbList2 && Object.keys(tbList2).length)
-      console.log(tbList2)
+      console.log(JSON.stringify(tbList2, null, 2))
     })
   })
 
