16119fbf3a7b48f4519bb8f92b3055fe4f56b2c9
waiting <1661926154@qq.com>
2020-06-07 09:14:29 +0800

    feat(kmore): define getCurrentTime() to retrieve time from database
    only support pg, mysql, mysql2 now.

--- packages/kmore/src/index.ts
+++ packages/kmore/src/index.ts
@@ -1,6 +1,8 @@
 
 export * from './lib/index'
 export * from './lib/model'
+export * from './lib/helper'
+
 
 // export { genTbListFromType } from 'kmore-types'
 export { genTbListFromType } from './lib/tables'
---
+++ packages/kmore/src/lib/helper.ts
@@ -0,0 +1,53 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as Knex from 'knex'
+
+import { Config, EnumClient } from './model'
+
+
+export async function getCurrentTime(
+  dbh: Knex,
+  clientType: Config['client'],
+): Promise<string> {
+
+  const res = await dbh.raw('SELECT now() AS currenttime;')
+
+  if (typeof clientType === 'string' && clientType) {
+    switch (clientType) {
+      case EnumClient.pg:
+        return parseRespCommon(res)
+
+      case EnumClient.mysql:
+        return parseRespCommon(res)
+
+      case EnumClient.mysql2:
+        return parseRespMysql2(res)
+
+      default:
+        throw new TypeError(`Unsupported client value: '${clientType}' for getCurrentTime().
+        Only ${EnumClient.pg}, ${EnumClient.mysql}, ${EnumClient.mysql2} so far.
+        `)
+    }
+  }
+  else {
+    throw new TypeError('Value of client is empty or not string')
+  }
+}
+
+interface RespCommon {
+  rows: [{ currenttime: string }]
+}
+function parseRespCommon(res: RespCommon): string {
+  return res.rows[0].currenttime
+}
+
+interface RespMysql2 extends Array<unknown> {
+  0: [{ currenttime: string }]
+  1: unknown
+  length: 2
+}
+function parseRespMysql2(res: RespMysql2): string {
+  return res && res[0] && res[0][0] && res[0][0].currenttime
+    ? res[0][0].currenttime
+    : ''
+}
+
