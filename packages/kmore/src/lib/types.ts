9397db137f731c4e03144c4529c30317f4daebb5
waiting <1661926154@qq.com>
2021-04-26 18:47:53 +0800

    feat(kmore): add event arguments interfaces
    - OnQueryArgData
    - OnQueryErrorArgData
    - OnQueryErrorArgErr

--- packages/kmore/src/lib/types.ts
+++ packages/kmore/src/lib/types.ts
@@ -30,3 +30,51 @@ export type TbQueryBuilder<TRecord> = () => Knex.QueryBuilder<TRecord, TRecord[]
 //     ? QueryBuilderExt<TRecord>
 //     : QueryBuilderExt<Omit<TRecord, KeyExcludeOptional extends void ? never : KeyExcludeOptional>>
 
+
+export interface OnQueryArgData {
+  __knexUid: string
+  __knexTxId: undefined | string
+  method: string // 'select'
+  options: Record<string, unknown>
+  timeout: boolean
+  cancelOnTimeout: boolean
+  bindings: unknown[]
+  __knexQueryUid: string // 'mXxtvuJLHkZI816UZic57'
+  sql: string
+}
+
+export interface OnQueryErrorArgData {
+  __knexUid: string // "__knexUid2"
+  __knexTxId: undefined | string
+  queryContext: unknown
+  method: string
+  options: Record<string, unknown>
+  timeout: boolean
+  cancelOnTimeout: boolean
+  bindings: unknown[]
+  __knexQueryUid: string // 'rhS1Gw1-uA79HFgn1Ob9g'
+  sql: string // 'select "*x" from "tb_user"'
+}
+
+export interface OnQueryErrorArgErr {
+  code: string // '42703'
+  column: unknown
+  constraint: unknown
+  dataType: unknown
+  detail: unknown
+  file: string // 'parse_relation.c'
+  hint: unknown
+  internalPosition: unknown
+  internalQuery: unknown
+  length: number
+  line: string // '3514'
+  message?: string
+  name: 'error'
+  position: string // '8'
+  routine: string // 'errorMissingColumn'
+  schema: unknown
+  severity: string // 'ERROR'
+  stack?: string
+  table: unknown
+  where: unknown
+}
