80aba2b5a8e0de2931e576a25828776bf1417c8c
waiting <1661926154@qq.com>
2021-07-08 11:30:22 +0800

    feat(midway-component-kmore): export class KmoreComponent, TracerKmoreComponent

--- packages/midway-component-kmore/src/index.ts
+++ packages/midway-component-kmore/src/index.ts
@@ -4,8 +4,8 @@ import { DbManager } from './lib/db-man'
 export { AutoConfiguration as Configuration } from './configuration'
 export { DbManager }
 export * from './lib/types'
-export type { KmoreComponent } from './lib/kmore'
-export type { TracerKmoreComponent } from './lib/tracer-kmore'
+export { KmoreComponent } from './lib/kmore'
+export { TracerKmoreComponent } from './lib/tracer-kmore'
 
 export {
   getCurrentTime,
