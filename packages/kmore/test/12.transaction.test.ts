b7a5fcc9b2839672806bcfd25d29bcc0da10cf7c
waiting <1661926154@qq.com>
2021-12-07 11:18:56 +0800

    test(kmore): add trx rollback

--- packages/kmore/test/12.transaction.test.ts
+++ packages/kmore/test/12.transaction.test.ts
@@ -27,7 +27,7 @@ describe(filename, () => {
     }
   })
 
-  describe('Should transaction works', () => {
+  describe('Should transaction work', () => {
     it('transaction', async () => {
       const { dbh } = km
       const trx = await dbh.transaction()
@@ -37,25 +37,47 @@ describe(filename, () => {
         .forUpdate()
         .select('*')
         .where('uid', 1)
-        .then((rows) => {
-          void trx.commit()
-          return rows
-        })
-        .catch(() => {
-          void trx.rollback()
-          return []
-        })
+        .then()
+
+      await trx.commit()
       assert(ret && Array.isArray(ret))
       assert(ret.length === 1)
+    })
 
-      const tbUser2 = km.refTables.ref_tb_user()
-      const ret2 = await tbUser2
-        .select('*')
-        .catch(() => {
-          void trx.rollback()
-          return []
+    it('transaction rollback', async () => {
+      const { dbh } = km
+      const trx = await dbh.transaction()
+      const tbUser = km.refTables.ref_tb_user()
+      const uid2 = 2
+      const ret = await tbUser
+        .transacting(trx)
+        .forUpdate()
+        .update('uid', 99)
+        .where('uid', uid2)
+        .returning('uid')
+        .then((uids) => {
+          assert(uids && uids.length === 1 && uids[0] === uid2, uids.toString())
         })
+
+      await trx.rollback()
+
+      const ret2 = await tbUser
+        .transacting(trx)
+        .forUpdate()
+        .select('*')
+        .where('uid', 1)
+        .then()
       assert(ret2 && Array.isArray(ret2))
+      assert(ret2.length === 1)
+
+      const ret3 = await tbUser
+        .transacting(trx)
+        .forUpdate()
+        .select('*')
+        .where('uid', uid2)
+        .then()
+      assert(ret3 && Array.isArray(ret3))
+      assert(ret3.length === 0)
     })
 
     it('transaction subscription', (done) => {
