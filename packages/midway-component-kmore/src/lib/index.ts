b5745dac96020fdf7481b3b85699b9e71364c4c5
waiting <1661926154@qq.com>
2022-04-01 17:15:15 +0800

    fix(midway-component-kmore): export unsubscribeEventFuncOnResFinish()

--- packages/midway-component-kmore/src/lib/index.ts
+++ packages/midway-component-kmore/src/lib/index.ts
@@ -3,5 +3,8 @@ export * from './config'
 export * from './types'
 export { DbManager } from './db-man'
 export { KmoreComponent } from './kmore.component'
-export { TracerKmoreComponent } from './tracer-kmore'
+export {
+  TracerKmoreComponent,
+  unsubscribeEventFuncOnResFinish,
+} from './tracer-kmore'
 
