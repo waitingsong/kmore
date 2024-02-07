3a01e761d6aed0087f3cb38dd331da5e3a48a01b
waiting <1661926154@qq.com>
2024-02-07 11:42:52 +0800

    chore(kmore): export wrapIdentifier()

--- packages/kmore/src/lib/index.ts
+++ packages/kmore/src/lib/index.ts
@@ -3,6 +3,7 @@ export * from './kmore.js'
 export * from './types.js'
 export * from './builder.types.js'
 export * from './propagation.types.js'
+export { wrapIdentifier } from './helper.js'
 
 export { initKmoreEvent } from './config.js'
 export {
