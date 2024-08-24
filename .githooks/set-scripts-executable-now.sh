waitingsong/pgmq-js

chore: squash merge

.githooks/set-scripts-executable-now.sh
@@ -1,0 +1,11 @@
+#!/bin/bash
+set +e
+
+dirs=".scripts .github src bin"
+
+for dir in $dirs; do
+  [[ -f "$dir" ]] && find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II chmod a+x I
+done
+
+set -e
+

.githooks/set-scripts-executable.sh
@@ -4,9 +4,7 @@ set +e
 dirs=".scripts .github"
 
 for dir in $dirs; do
-  find $dir -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
-  find $dir -type f -iname "*.mjs" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
-  find $dir -type f -iname "*.mts" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+  [[ -f "$dir" ]] && find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
 done
 
 find .githooks -type f -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I

waitingsong/kmore

.githooks/set-scripts-executable-now.sh
@@ -1,0 +1,11 @@
+#!/bin/bash
+set +e
+
+dirs=".scripts .github src bin"
+
+for dir in $dirs; do
+  [[ -d "$dir" ]] && find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II chmod a+x I
+done
+
+set -e
+

.githooks/set-scripts-executable.sh
@@ -4,9 +4,7 @@ set +e
 dirs=".scripts .github"
 
 for dir in $dirs; do
-  find $dir -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
-  find $dir -type f -iname "*.mjs" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
-  find $dir -type f -iname "*.mts" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+  [[ -d "$dir" ]] && find $dir -type f \( -iname "*.sh" -o -iname "*.mjs" -o -iname "*.mts" \) -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
 done
 
 find .githooks -type f -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I